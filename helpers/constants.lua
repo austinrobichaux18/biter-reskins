bd481a047e43fee79c9e375b21775478e978060f
austinrobichaux18 <54606588+austinrobichaux18@users.noreply.github.com>
2026-06-23 15:47:22 -0500

    feat: add portal2 skin, remove not working worm death animation, update roadmap

--- helpers/constants.lua
+++ helpers/constants.lua
@@ -2,8 +2,8 @@ spawner_skins = { 'grey-tent', 'horn-tent', 'default' }
 spawner_default_skin_biter = 'horn-tent'
 spawner_default_skin_spitter = 'grey-tent'
 
-worm_skins = { 'zerg', 'default' }
-worm_default_skin = 'zerg'
+worm_skins = { 'zerg', 'portal2', 'default' }
+worm_default_skin = 'portal2'
 
 unit_skins = { 'goblin-and-orc', 'piggy-and-eye', 'goblin', 'orc', 'piggy', 'eye', 'default' }
 unit_default_skin_biter = 'goblin-and-orc'
